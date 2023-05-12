3d533acdefa81cad9faa7e2c593ce20a63fbca83
Carlos Espinal <cespinalb@gmail.com>
2023-05-12 13:50:01 -0600

    actualizacion

--- src/lab4p2_equipo2/Lab4P2_Equipo2.java
+++ src/lab4p2_equipo2/Lab4P2_Equipo2.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package lab4p2_equipo2;
 
-/**
- *
- * @author cespi
- */
 public class Lab4P2_Equipo2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Carlos Espinal 12151049
+        //Akeem Ieong Ham 12241250
+        
+        
     }
     
 }
--- src/lab4p2_equipo2/seresVivos.java
+++ src/lab4p2_equipo2/seresVivos.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab4p2_equipo2;
 
-/**
- *
- * @author cespi
- */
 public class seresVivos {
+    protected String name;
+    protected String gSanguineo;
+    protected String genero;
+    protected double altura;
+    protected double peso;
+
+    public seresVivos(String name, String gSanguineo, String genero, double altura, double peso) {
+        this.name = name;
+        this.gSanguineo = gSanguineo;
+        this.genero = genero;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getgSanguineo() {
+        return gSanguineo;
+    }
+
+    public void setgSanguineo(String gSanguineo) {
+        this.gSanguineo = gSanguineo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "seresVivos{" + "name=" + name + ", gSanguineo=" + gSanguineo + ", genero=" + genero + ", altura=" + altura + ", peso=" + peso + '}';
+    }
+    
+    
     
 }
